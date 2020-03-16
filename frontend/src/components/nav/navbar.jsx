027817546061abfff9d76e8e8938e7c40e45efef
zeke <emostov@middlebury.edu>
2020-03-15 19:53:52 -0700

    setup sidebar

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -42,7 +42,7 @@ class NavBar extends React.Component {
         <>
           <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="#home">COVID415</Navbar.Brand>
-            <Nav className="mr-auto">
+            <Nav className="mr-auto justify-content-space-between">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#features">Features</Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
---
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class SideBar extends Component {
+
+
+  render() {
+    return (
+      <div className='sidebar-container'>
+
+      </div>
+    )
+  }
+}
