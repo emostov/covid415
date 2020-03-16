1210b8a8bb62451d341b061f5120fc007bf316f6
Tarik <tariksnow37@gmail.com>
2020-03-16 16:02:10 -0700

    connect frontend with backend

--- frontend/src/components/tasks/task_index_container.js
+++ frontend/src/components/tasks/task_index_container.js
@@ -1,12 +1,17 @@
 import { connect } from 'react-redux'
 import TaskIndex from './task_index';
+import { fetchTasks } from '../../actions/task_action'
 
-const mSTP = state => ({
+const mSTP = state => {
+    return {
+        tasks: Object.values(state.tasks)
+    }
+}
 
-});
+const mDTP = dispatch => {
+    return {
+        fetchTasks: () => dispatch(fetchTasks())
+    }
+};
 
-const mDTP = dispatch => ({
-
-});
-
-export default (mSTP, mDTP)(TaskIndex);
+export default connect(mSTP, mDTP)(TaskIndex);
