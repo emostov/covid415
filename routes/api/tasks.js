1d4cd98b5126cf39dee2ea58f83b926580996138
zeke <emostov@middlebury.edu>
2020-03-17 14:19:42 -0700

    start making route

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -1,45 +1,91 @@
-const geocodeUtil = require('../../util/geocode_util');
-const express = require("express");
-const router = express.Router();
+const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+
+const geocodeUtil = require('../../util/geocode_util');
 const Task = require('../../models/Task');
 const validateTaskInput = require('../../validation/tasks');
 
-router.get("/test", (req, res) => res.json({ msg: "This is the tasks route" }));
+const router = express.Router();
+
+
+router.get('/test', (req, res) => res.json({ msg: 'This is the tasks route' }));
 
 router.get('/', (req, res) => {
-    Task.find()
-        .sort({ createdAt: -1 })
-        .then(tasks => res.json(tasks))
-        .catch(err => res.status(404).json({ notasksfound: 'No tasks found' }));
+  Task.find()
+    .sort({ createdAt: -1 })
+    .then((tasks) => res.json(tasks))
+    .catch((err) => res.status(404).json({ notasksfound: 'No tasks found' }));
 });
 
 router.post('/',
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-      const { errors, isValid } = validateTaskInput(req.body);
-  
-      if (!isValid) {
-        return res.status(400).json(errors);
-      }
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateTaskInput(req.body);
 
-      geocodeUtil.parseAddress(req.body.deliveryAddress).then(
-        (latLongArr) => {
-        const newTask = new Task ({
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    geocodeUtil.parseAddress(req.body.deliveryAddress).then(
+      (latLongArr) => {
+        const newTask = new Task({
           type: req.body.type,
           details: req.body.details,
           requester: req.user.id,
           deliveryAddress: req.body.deliveryAddress,
           deliveryLatLong: latLongArr,
-          deliveryInstructions: req.body.deliveryInstructions
-        })
+          deliveryInstructions: req.body.deliveryInstructions,
+        });
 
-        newTask.save().then(task => res.json(task));
+        newTask.save().then((task) => res.json(task));
+      },
+    );
+  });
 
-        }
-      );
+router.patch('/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateTaskInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
     }
-  );
+
+    const {
+      type,
+      details,
+      deliveryAddress,
+      deliveryLatLong,
+      deliveryInstructions,
+      requester,
+      volunteer,
+      status,
+      createdAt,
+      updatedAt
+    } = req.body;
+
+    Task.findById(req.params.id)
+      .then((task) => {
+        const updatedTask = {
+          ...task,
+          type,
+          details,
+          // deliveryAddress,
+          // deliveryLatLong,
+          deliveryInstructions,
+          // requester,
+          volunteer,
+          status,
+          // createdAt,
+          updatedAt,
+        };
+        updatedTask.save()
+          .then((savedTask) => res.json(savedTask))
+          .catch((err) => console.log(err));
+      });
+
+  });
+
 
 module.exports = router;
