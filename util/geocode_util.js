5d0c58675d2e7323661e3c14beebc55a69d4fc68
zeke <emostov@middlebury.edu>
2020-03-17 09:00:32 -0700

    revise geocodeKey imports

--- util/geocode_util.js
+++ util/geocode_util.js
@@ -1,13 +1,13 @@
-const axios = require ('axios');
-const geocodeKey = require('../config/keys_geocode');
+const axios = require('axios');
+// const geocodeKey = require('../config/keys_geocode');
+const { geocodeKey } = require('../config/keys');
 
 module.exports = {
-    parseAddress: address => {
-        let addr = address.split(' ').join('+');
+  parseAddress: (address) => {
+    const addr = address.split(' ').join('+');
 
-        return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addr}&key=${geocodeKey.key}`)
-            .then(res => (
-                Object.values(res.data.results[0].geometry.location))
-            )
-    }
-}
+    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addr}&key=${geocodeKey.key}`)
+      .then((res) => (
+        Object.values(res.data.results[0].geometry.location)));
+  },
+};
