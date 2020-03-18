02e39187329873108540e7c58df42e46d82e45ca
jayfang88 <jayfang88@gmail.com>
2020-03-18 00:58:49 -0700

    fix current user id

--- frontend/src/components/sidebar/sidebar_container.js
+++ frontend/src/components/sidebar/sidebar_container.js
@@ -3,9 +3,18 @@ import SideBar from './sidebar';
 import { updateTask } from '../../actions/task_actions';
 import { openModal, closeModal } from '../../actions/modal_actions';
 
-const mSTP = state => ({
-    currentUserId: state.session.user.id || null,
-});
+const mSTP = state => {
+    let currentUserId;
+    if (state.session.user) {
+        currentUserId = state.session.user.id
+    } else {
+        currentUserId = undefined
+    }
+
+    return {
+        currentUserId: currentUserId
+    }
+};
 
 const mDTP = dispatch => ({
     updateTask: () => dispatch(updateTask()),
