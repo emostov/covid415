59f84ceb673759379e1d4ec01670391df10b0cdb
zeke <emostov@middlebury.edu>
2020-03-20 12:07:44 -0700

    refactor keys

--- frontend/src/config/keys_front.js
+++ frontend/src/config/keys_front.js
@@ -1,3 +1,10 @@
 
-module.exports = process.env.NODE_ENV === 'production'
-  ? (require('./keys_prod_front.js')) : (require('./keys_dev_front.js'));
+// module.exports = process.env.NODE_ENV === 'production'
+//   ? (require('./keys_prod_front.js')) : (require('./keys_dev_front.js'));
+
+module.exports = {
+  geocodeKey: 'AIzaSyDsGMoUwaDuVAoiYUH7edAUr2i_adeeH-s',
+  mapBoxPublicKey: 'pk.eyJ1IjoiaGltdXJheDN4IiwiYSI6ImNrN3UwYmptNjB3eGEzZnB1MGZyMXN4eGIifQ.KzlIQPQU2VgrhC7y_8eMcw',
+}
+
+
