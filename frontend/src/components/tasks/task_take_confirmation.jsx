79733ac317ac64ef776684ac4bf0df6c73a4221a
Andrew Howell <andhudhow@gmail.com>
2020-03-20 16:18:24 -0700

    update copy on delivery confirmation

--- frontend/src/components/tasks/task_take_confirmation.jsx
+++ frontend/src/components/tasks/task_take_confirmation.jsx
@@ -6,15 +6,13 @@ import '../../styles/task_update.scss'
 
 class TaskTakeConfirmation extends React.Component {
     render() {
-        const { task } = this.props
-
         return (
             <div className="modal-child-confirm-delivery">
             <div className="delivery-header-container">
-                <div className="delivery-header">You're good to go!</div>
+                <div className="delivery-header">Delivery accepted!</div>
             </div>
             <div className="delivery-confirm-body">
-                Delivery request successfully added to 'My Deliveries'.
+                Check 'My Deliveries' for delivery details.
             </div>
         </div>
         )
