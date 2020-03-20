bf9c8af3cedc3fbf235dbccd026bcd45dd1e1b82
zeke <emostov@middlebury.edu>
2020-03-20 11:46:41 -0700

    remove useles imports

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
--- frontend/src/components/session/login_form.js
+++ frontend/src/components/session/login_form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Form, Row, Col, Button, Card, Container, Alert } from 'react-bootstrap'
+import { Form, Button, Card, Alert } from 'react-bootstrap'
 
 import '../../styles/session_form.scss'
 
