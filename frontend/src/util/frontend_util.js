e1a9f4e48ca3d4883f18afe80f1efe1280fdb06e
Tarik <tariksnow37@gmail.com>
2020-03-20 23:44:15 -0700

    wrote first function

--- frontend/src/util/frontend_util.js
+++ frontend/src/util/frontend_util.js
@@ -2,5 +2,20 @@ module.exports = {
     parseDistance: (num) => {
         let multiplier = Math.pow(10, 2);
         return Math.round(num * multiplier) / multiplier;
-    }
+    },
+    sortDistances: (objectArr) => {
+        const distances = [];
+        for (let i = 0; i < objectArr.length; i++) {
+            distances.push(objectArr[i].distance)
+        }
+
+        const sorted = distances.sort()
+        return sorted
+    },
+    // sortTasks: (sortedArr, Objects) => {
+    //     for(let i = 0; i < sortedArr.length; i++) {
+    //         sortedArr[i]
+    //     }
+    // }
 }
+
