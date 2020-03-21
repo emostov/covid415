12ea4fe24dc26469b567e9f118c5880d662f29a2
zeke <emostov@middlebury.edu>
2020-03-20 21:19:24 -0700

    make seed users and tasks function

--- seeds/generate_seed_json.js
+++ seeds/generate_seed_json.js
@@ -1,23 +1,68 @@
-{
+/* eslint-disable no-console */
+// const axios = require('axios');
+const geocodeUtil = require('./../util/geocode_util');
+const Task = require('../models/Task');
+const User = require('../models/User');
+
+const {
   firstNames,
   lastNames,
   deliveryInstructions,
   details,
   addresses,
-  } = require('./seed_data.js');
+} = require('./seed_data.js');
 
-const generateUserObjs = () => {
-  cosnt users = [];
-  for (let i = 0, i < addresses.length, i += 1) {
-    const r = Math.floor(Math.random * 1000);
+
+
+const generateUserObjs = (n) => {
+  const users = [];
+
+  for (let i = 0; i < n; i += 1) {
+    const r = Math.floor(Math.random() * 1000);
     const newUser = {
-      firstName: firstNames[r % firstNames.length],
+      firstName: firstNames[r % (firstNames.length - 1)],
       lastName: lastNames[r % lastNames.length],
       email: `randEmail${i}@gmail.com`,
-      password: 'password'
+      password: 'password',
     };
 
     users.push(newUser);
   }
-  return users
-}
+  return users;
+};
+
+const types = ['food', 'medicine', 'other'];
+
+const seedUsersAndTasks = (n) => {
+  
+  const users = generateUserObjs(n);
+  for (let i = 0; i < users.length; i += 1) {
+    const r = Math.floor(Math.random() * 1000);
+    const newUser = new User(users[i]);
+    const deliverAdd = addresses[r % addresses.length];
+    newUser.save()
+      .then((savedUser) => {
+        console.log(savedUser);
+        geocodeUtil.parseAddress(deliverAdd)
+          .then((gMapsResponse) => {
+            const taskObj = {
+              requester: savedUser,
+              status: 0,
+              type: types[r % 3],
+              details: details[r % details.length],
+              deliveryInstructions: deliveryInstructions[r % deliveryInstructions.length],
+              deliveryAddress: deliverAdd,
+              deliveryLatLong: Object.values(gMapsResponse.data.results[0].geometry.location),
+              deliveryNeighborhood: gMapsResponse.data.results[0].address_components[2].short_name,
+            };
+            console.log(taskObj);
+            taskObj.save()
+              .then((t) => console.log(t));
+          })
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  }
+};
+
+console.log(seedUsersAndTasks(3));
--- seeds/seed_data.js
+++ seeds/seed_data.js
@@ -1,23 +1,23 @@
-export const firstNames = [
+const firstNames = [
   'Amanda', 'Dianna', 'Gianna', 'Jack', 'Tyrell', 'Sue', 'Jen', 'Hal', 'Jesus',
   'Kevin', 'Nina', 'Rose', 'Kai', 'Kaiwan', 'Fin', 'Tabatha', 'Victor', 'Moses',
   'Mads', 'Mikkel', 'Magnus', 'Raphie', 'Ariella', 'Keith', 'Emily', 'Daisy',
   'Hillary', 'Jim',
 ];
 
-export const lastNames = [
+const lastNames = [
   'Garcia', 'Juarez', 'Fineman', 'Goldman', 'Braudy',
   'Jung', 'Lee', 'Tran', 'Ha', 'Grobchinsky', 'Chomsky', 'Bak', 'Hernandez',
   'Peterson', 'Braunsmans', 'Sagan',
 ];
 
-export const deliveryInstructions = [
+const deliveryInstructions = [
   'Call me once out front', 'Text when you arrive',
   'Please throw it over the fence', 'Leaveo on porch',
   'Please do not call and just leave outside', 'Check if the lights are on',
 ];
 
-export const details = [
+const details = [
   'I need a full list of groceries, I will text details once confirmed',
   'My medication needs to be picked up thanks :)',
   'I have a order waiting at the medical supplies store that needs pickup',
@@ -30,7 +30,7 @@ export const details = [
   'What about a pet salmander? You just some groceries',
 ];
 
-export const addresses = [
+const addresses = [
   '2311 Broadway Street, San Francisco, CA',
   '2400 Fulton Street, San Francisco, CA',
   '245 Hyde Street, San Francisco',
@@ -57,3 +57,11 @@ export const addresses = [
   '1333 Silliman St, San Francisco, CA 94134',
   '320 Silliman St, San Francisco, CA 94134',
 ];
+
+module.exports = {
+  addresses,
+  firstNames,
+  lastNames,
+  details,
+  deliveryInstructions,
+}
