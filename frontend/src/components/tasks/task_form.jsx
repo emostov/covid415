093439b9b948b515784ca2a71018a2e4f232e439
zeke <emostov@middlebury.edu>
2020-03-21 10:15:19 -0700

    modify modal close to try and only close if now errors

--- frontend/src/components/tasks/task_form.jsx
+++ frontend/src/components/tasks/task_form.jsx
@@ -9,120 +9,123 @@ import Other from '../../public/other.png';
 import keys from '../../config/keys_mapbox';
 
 class TaskForm extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = this.props.task
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+    this.state = this.props.task
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
-    componentWillReceiveProps(nextProps) {
-        this.setState({ errors: nextProps.errors })
-    }
+  componentWillReceiveProps(nextProps) {
+    this.setState({ errors: nextProps.errors })
+  }
 
-    update(field) {
-        return e => this.setState({ [field]: e.currentTarget.value })
-    }
+  update(field) {
+    return e => this.setState({ [field]: e.currentTarget.value })
+  }
 
-    renderErrors() {
-        return (
-            <ul className='errors-list'>
-                {Object.values(this.state.errors).map((error, i) => (
-                    <li key={`error=${i}`}>
-                        <Alert variant='warning'>
-                            {error}
-                        </Alert>
-                    </li>
-                ))}
-            </ul>
-        )
-    }
+  renderErrors() {
+    return (
+      <ul className='errors-list'>
+        {Object.values(this.state.errors).map((error, i) => (
+          <li key={`error=${i}`}>
+            <Alert variant='warning'>
+              {error}
+            </Alert>
+          </li>
+        ))}
+      </ul>
+    )
+  }
 
-    handleSubmit(e) {
-        e.preventDefault();
+  handleSubmit(e) {
+    e.preventDefault();
 
-        this.props.createNewTask(this.state)
-            .then(() => this.props.fetchTasks())
-            .then((res))
-            .then(() => this.props.closeModal())
-    }
+    this.props.createNewTask(this.state)
+      .then(() => this.props.fetchTasks())
+      .then(() => {
+        if (!this.state.errors.length) {
+          this.props.closeModal();
+        }
+      })
+  }
 
-    render() {
-        return (
-            <div className='task-form-container'>
-                <Card.Title className='task-form-title'><strong>Under quarantine? Request a delivery.</strong></Card.Title>
-                {this.renderErrors()}
-                <Form className='task-form'>
-                    <Form.Group>
-                        <Form.Label className='task-form-label'>What are you requesting?</Form.Label>
-                        <div className='task-type-group'>
-                            <Form.Group className='task-form-type'>
-                                <label>
-                                    <input type="radio" name='task-type' onChange={this.update('type')} value='Food' />
-                                    <img className='radio-img' src={Food} alt="grocery-pic" />
-                                    <div className='task-type-label'>Food</div>
-                                </label>
-                                <label>
-                                    <input type="radio" name='task-type' onChange={this.update('type')} value='Medicine' />
-                                    <img className='radio-img' src={Medicine} alt="medicine-pic" />
-                                    <div className='task-type-label'>Medicine</div>
-                                </label>
-                                <label>
-                                    <input type="radio" name='task-type' onChange={this.update('type')} value='Other' />
-                                    <img className='radio-img' src={Other} alt="other-pic" />
-                                    <div className='task-type-label'>Other</div>
-                                </label>
-                            </Form.Group>
-                        </div>
-                    </Form.Group>
-
-                    <Form.Group>
-                        <Form.Label className='task-form-label'>Where is this being delivered to?</Form.Label>
-                        <GooglePlacesAutocomplete
-                            autocompletionRequest={{
-                                bounds: [
-                                    { lat: 37.6, lng: -122.54 },
-                                    { lat: 37.9, lng: -122.34 }
-                                ],
-                                componentRestrictions: {
-                                    country: ['us']
-                                }
-                            }}
-                            placeholder='825 Battery Street, San Francisco, CA 94111'
-                            onSelect={({ description }) => (
-                                this.setState({ deliveryAddress: description })
-                            )}
-                            suggestionsClassNames={{
-                                container: 'suggestion-container',
-                                suggestion: 'suggestion'
-                            }}
-                        // loader={<img src={loader} />}
-                        />
-                    </Form.Group>
-
-                    <Form.Group>
-                        <Form.Label className='task-form-label'>Details</Form.Label>
-                        <Form.Control
-                            as='textarea'
-                            placeholder='Please bring me some fuji apples and Sour Diesel'
-                            onChange={this.update('details')}
-                            className='task-form-input-long'
-                        />
-                    </Form.Group>
-
-                    <Form.Group>
-                        <Form.Label className='task-form-label'>Please provide additional delivery instructions</Form.Label>
-                        <Form.Control
-                            placeholder='Leave it on the front porch'
-                            onChange={this.update('deliveryInstructions')}
-                            className='task-form-input-short'
-                        />
-                    </Form.Group>
-                </Form>
-                <Button className='task-form-submit' onClick={this.handleSubmit}>Request Delivery</Button>
+  render() {
+    return (
+      <div className='task-form-container'>
+        <Card.Title className='task-form-title'><strong>Under quarantine? Request a delivery.</strong></Card.Title>
+        {this.renderErrors()}
+        <Form className='task-form'>
+          <Form.Group>
+            <Form.Label className='task-form-label'>What are you requesting?</Form.Label>
+            <div className='task-type-group'>
+              <Form.Group className='task-form-type'>
+                <label>
+                  <input type="radio" name='task-type' onChange={this.update('type')} value='Food' />
+                  <img className='radio-img' src={Food} alt="grocery-pic" />
+                  <div className='task-type-label'>Food</div>
+                </label>
+                <label>
+                  <input type="radio" name='task-type' onChange={this.update('type')} value='Medicine' />
+                  <img className='radio-img' src={Medicine} alt="medicine-pic" />
+                  <div className='task-type-label'>Medicine</div>
+                </label>
+                <label>
+                  <input type="radio" name='task-type' onChange={this.update('type')} value='Other' />
+                  <img className='radio-img' src={Other} alt="other-pic" />
+                  <div className='task-type-label'>Other</div>
+                </label>
+              </Form.Group>
             </div>
-        )
-    }
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label className='task-form-label'>Where is this being delivered to?</Form.Label>
+            <GooglePlacesAutocomplete
+              autocompletionRequest={{
+                bounds: [
+                  { lat: 37.6, lng: -122.54 },
+                  { lat: 37.9, lng: -122.34 }
+                ],
+                componentRestrictions: {
+                  country: ['us']
+                }
+              }}
+              placeholder='825 Battery Street, San Francisco, CA 94111'
+              onSelect={({ description }) => (
+                this.setState({ deliveryAddress: description })
+              )}
+              suggestionsClassNames={{
+                container: 'suggestion-container',
+                suggestion: 'suggestion'
+              }}
+            // loader={<img src={loader} />}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label className='task-form-label'>Details</Form.Label>
+            <Form.Control
+              as='textarea'
+              placeholder='Please bring me some fuji apples and Sour Diesel'
+              onChange={this.update('details')}
+              className='task-form-input-long'
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label className='task-form-label'>Please provide additional delivery instructions</Form.Label>
+            <Form.Control
+              placeholder='Leave it on the front porch'
+              onChange={this.update('deliveryInstructions')}
+              className='task-form-input-short'
+            />
+          </Form.Group>
+        </Form>
+        <Button className='task-form-submit' onClick={this.handleSubmit}>Request Delivery</Button>
+      </div>
+    )
+  }
 };
 
 export default TaskForm;
