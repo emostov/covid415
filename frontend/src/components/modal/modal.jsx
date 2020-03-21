35ab753586b42491388e5be1ef2d400e29388897
Andrew Howell <andhudhow@gmail.com>
2020-03-20 17:20:20 -0700

    resolve merge conflicts

--- frontend/src/components/modal/modal.jsx
+++ frontend/src/components/modal/modal.jsx
@@ -5,50 +5,63 @@ import TaskUpdateContainer from '../tasks/task_update_container';
 import TaskFormContainer from '../tasks/task_form_container';
 import TaskTakeConfirmation from '../tasks/task_take_confirmation';
 import TaskDetailsContainer from '../tasks/task_details_container';
-import CompleteTakeConfirmation from '../tasks/task_complete_confirmation';
+import CompleteTakeConfirmation from '../tasks/task_complete_confirmation'
 import '../../styles/modal.scss';
 
-const Modal = ({modal, closeModal}) => {
-    if(!modal) {
-        return null
+class Modal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.close = this.close.bind(this);
     }
 
-    let component;
-    let childClass;
+    close() {
+        this.props.closeModal();
+    }
 
-    switch (modal.modal) {
-        case 'taskform':
-            component = <TaskFormContainer />;
-            childClass = "modal-child-taskform"
-            break;
-        case 'status':
-            component = <TaskUpdateContainer taskId={modal.taskId}/>;
-            childClass = "modal-child-status"
-            break;
-        case 'details':
-            component = <TaskDetailsContainer taskId={modal.taskId}/>;
-            childClass = "modal-child-status"
-            break;
-        case 'takeTaskConfirmed':
-            component = <TaskTakeConfirmation />;
-            childClass = "modal-child-taketask"
-            break;
-        case 'completeTaskConfirmed':
-            component = <CompleteTakeConfirmation />;
-            childClass = "modal-child-taketask"
-            break;
-        default: 
+    render() {
+        const { modal, closeModal } = this.props;
+
+        if(!modal) {
             return null;
-    }
-    
-    
-    return (
-        <div className="modal-background" onClick={closeModal}>
-            <div className={childClass} onClick={e => e.stopPropagation()}>
-               { component }
+        }
+
+        let component;
+        let childClass;
+
+        switch (modal.modal) {
+            case 'taskform':
+                component = <TaskFormContainer />;
+                childClass = "modal-child-taskform"
+                break;
+            case 'status':
+                component = <TaskUpdateContainer taskId={modal.taskId}/>;
+                childClass = "modal-child-status"
+                break;
+            case 'details':
+                component = <TaskDetailsContainer taskId={modal.taskId}/>;
+                childClass = "modal-child-status"
+                break;
+            case 'takeTaskConfirmed':
+                component = <TaskTakeConfirmation />;
+                childClass = "modal-child-taketask"
+                break;
+            case 'completeTaskConfirmed':
+                component = <CompleteTakeConfirmation />;
+                childClass = "modal-child-taketask"
+                break;
+            default: 
+                return null;
+        }
+        
+        
+        return (
+            <div className="modal-background" onClick={closeModal}>
+                <div className={childClass} onClick={e => e.stopPropagation()}>
+                { component }
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 const msp = (state) => {
