92b5f5fb7c06991bd5f910ece5ff3e919485105b
zeke <emostov@middlebury.edu>
2020-03-21 17:12:27 -0700

    comment out logger

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -8,8 +8,8 @@ const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
-    // applyMiddleware(thunk)
+    // applyMiddleware(thunk, logger)
+    applyMiddleware(thunk)
   )
 );
 
