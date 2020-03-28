f8fc81e735f62ce9cd44529f7f939fed94167736
Tarik <tariksnow37@gmail.com>
2020-03-28 13:09:49 -0700

    add Phone to model

--- models/User.js
+++ models/User.js
@@ -19,6 +19,10 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
   date: {
     type: Date,
     default: Date.now,
