e90a49fbcc24929dc29dcb208229ee9d6c61f8aa
Tarik <tariksnow37@gmail.com>
2020-03-28 13:14:26 -0700

    add phone validation for validators

--- validation/register.js
+++ validation/register.js
@@ -8,6 +8,7 @@ module.exports = function validateRegisterInput(dat) {
   data.firstName = validText(data.firstName) ? data.firstName : '';
   data.lastName = validText(data.lastName) ? data.lastName : '';
   data.email = validText(data.email) ? data.email : '';
+  data.phoneNumber = validText(data.phoneNumber) ? data.phoneNumber : '';
   data.password = validText(data.password) ? data.password : '';
   data.password2 = validText(data.password2) ? data.password2 : '';
 
@@ -27,13 +28,16 @@ module.exports = function validateRegisterInput(dat) {
     errors.lastName = 'Last name field is required';
   }
 
-
   if (Validator.isEmpty(data.email)) {
     errors.email = 'Email field is required';
   }
 
   if (!Validator.isEmail(data.email)) {
     errors.email = 'Email is invalid';
+  }
+
+  if (Validator.isEmpty(data.phoneNumber)) {
+    errors.phoneNumber = 'Phone Number field is required';
   }
 
   if (Validator.isEmpty(data.password)) {
