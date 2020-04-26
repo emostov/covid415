89af45e5d9d376dbde2ec02e72768d77652841a2
Andrew Howell <andhudhow@gmail.com>
2020-04-25 23:00:57 -0700

    enable logger only on dev

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,15 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 
 import rootReducer from '../reducers/root_reducer';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== "production") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+};
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
-    // applyMiddleware(thunk)
+    applyMiddleware(...middlewares)
   )
 );
 
