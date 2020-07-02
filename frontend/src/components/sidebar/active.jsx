03a41b158cc431c27ce19aa0393c4712fc774c1f
Tarik <tariksnow37@gmail.com>
2020-07-02 19:42:57 -0400

    cleanup code

--- frontend/src/components/sidebar/active.jsx
+++ frontend/src/components/sidebar/active.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import Card from './card';
-import frontendUtil from '../../util/frontend_util'
+import frontendUtil from '../../util/frontend_util';
 
 const ActiveSidebar = props => {
   const {
@@ -17,20 +17,20 @@ const ActiveSidebar = props => {
     session
   } = props;
 
-  const idSet = {}
-  const u3 = []
+  const idSet = {};
+  const u3 = [];
 
   active.forEach((task) => {
     if (idSet[task._id] === undefined && task.volunteer === currentUserId) {
       idSet[task._id] = true;
-      u3.push(task)
+      u3.push(task);
     }
   });
 
   let currentUserTasks = frontendUtil.sortDistances2(u3);
 
   return (
-    <div className="card-container">
+    <div className='card-container'>
       {
         session.isAuthenticated
           ?
@@ -56,13 +56,13 @@ const ActiveSidebar = props => {
               }
             </div>
           ) : (
-            <div className="card-container-demo">
-              <div className="card-demo-content">
-                <div className="head-demo">Hey Neighbor!</div>
-                <div className="card-demo-text">You must <Link to="/login">log in</Link> to view your deliveries.</div>
+            <div className='card-container-demo'>
+              <div className='card-demo-content'>
+                <div className='head-demo'>Hey Neighbor!</div>
+                <div className='card-demo-text'>You must <Link to='/login'>log in</Link> to view your deliveries.</div>
                 <br /><br />
-                <div className="head-demo">Want to be a volunteer?</div>
-                <div className="card-demo-text"><Link to="/signup">Sign up now</Link> and help keep the 415 alive.</div>
+                <div className='head-demo'>Want to be a volunteer?</div>
+                <div className='card-demo-text'><Link to='/signup'>Sign up now</Link> and help keep the 415 alive.</div>
               </div>
             </div>
           )
