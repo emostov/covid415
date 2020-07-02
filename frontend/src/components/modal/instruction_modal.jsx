09948b47f8997a51417596b712d0675851c82a32
Tarik <tariksnow37@gmail.com>
2020-07-02 19:01:40 -0400

    InstructionModal from class to functional component

--- frontend/src/components/modal/instruction_modal.jsx
+++ frontend/src/components/modal/instruction_modal.jsx
@@ -3,35 +3,32 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { closeModal } from '../../actions/modal_actions';
 
-
 import Logo from '../../public/COVID415.png';
-import TaskList from '../../public/tasklist.png'
+import TaskList from '../../public/tasklist.png';
 
-class InstructionModal extends React.Component {
-    render() {
-        return (
-            <div className="modal-welcome-container">
-                <div className="modal-welcome-heading">
-                    <img src={ Logo } className='welcome-logo' alt="welcome-logo" />
-                    <div className ='modal-welcome-header-text'>Welcome, neighbor! Read below to learn how COVID-415 works.</div>
+const InstructionModal = (props) => {
+    return (
+        <div className='modal-welcome-container'>
+            <div className='modal-welcome-heading'>
+                <img src={Logo} className='welcome-logo' alt='welcome-logo' />
+                <div className='modal-welcome-header-text'>Welcome, neighbor! Read below to learn how COVID-415 works.</div>
+            </div>
+            <div className='modal-welcome-body'>
+                <div className='welcome-section-1'>
+                    <div className='modal-body-header'>How to volunteer:</div>
+                    <div className='modal-body-text'>Sign up and browse delivery requests from neighbors nearby.</div>
+                    <br />
+                    <div className='modal-body-text'>Once you've claimed a delivery, we'll share your neighbor's contact information so you can get in touch.</div>
+                    <button className='continue-button' onClick={props.closeModal}>Continue </button>
                 </div>
-                <div className="modal-welcome-body">
-                        <div className="welcome-section-1">
-                            <div className="modal-body-header">How to volunteer:</div>
-                            <div className="modal-body-text">Sign up and browse delivery requests from neighbors nearby.</div>
-                            <br />
-                            <div className="modal-body-text">Once you've claimed a delivery, we'll share your neighbor's contact information so you can get in touch.</div>
-                            <button className="continue-button" onClick={this.props.closeModal}>Continue </button>
-                        </div>
-                        <div className="welcome-section-2 nomobile">
-                            <img src={TaskList} className="modal-splash" alt="task-map" />
-                            <div className="prototype-note">Please note: COVID-415 is currently in prototype. All requests are for demonstration purposes only.</div>
-                        </div>
+                <div className='welcome-section-2 nomobile'>
+                    <img src={TaskList} className='modal-splash' alt='task-map' />
+                    <div className='prototype-note'>Please note: COVID-415 is currently in prototype. All requests are for demonstration purposes only.</div>
                 </div>
             </div>
-        )
-    }
-};
+        </div>
+    );
+}
 
 
 const mDTP = dispatch => ({
