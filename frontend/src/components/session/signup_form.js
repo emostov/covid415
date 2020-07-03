0ac5ffa87ebc6e9e7e001982924b783fadd6218a
Tarik <tariksnow37@gmail.com>
2020-07-02 22:58:21 -0400

    update signupform to use hooks

--- frontend/src/components/session/signup_form.js
+++ frontend/src/components/session/signup_form.js
@@ -1,42 +1,44 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { withRouter } from 'react-router-dom';
 import { Form, Row, Col, Button, Card, Alert } from 'react-bootstrap';
 
-class SignupForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const SignupForm = (props) => {
+
+  const { login, errors, currentUser, history, signup } = props;
+
+  const [filterInput, setFilterInput] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    {
       email: '',
       firstName: '',
       lastName: '',
       phoneNumber: '',
       password: '',
       password2: '',
-      errors: {}
-    };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.clearedErrors = false;
-    this.demoLogin = this.demoLogin.bind(this);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.signedIn === true) {
-      this.props.history.push('/');
     }
+  );
 
-    this.setState({ errors: nextProps.errors })
-  }
+  useEffect(() => {
+    if (currentUser === true) {
+      history.push('/');
+    }
+  }, [currentUser]);
 
-  update(field) {
-    return e => this.setState({
-      [field]: e.currentTarget.value
-    });
-  }
+  const update = (e) => {
+    const { name, value } = e.target;
+    setFilterInput({ [name]: value });
+  };
 
-  handleSubmit(e) {
-    e.preventDefault();
-    const { email, firstName, lastName, phoneNumber, password, password2 } = this.state
+  const handleSubmit = () => {
+    const {
+      email,
+      firstName,
+      lastName,
+      phoneNumber,
+      password,
+      password2
+    } = filterInput;
+
     const user = {
       email,
       firstName,
@@ -46,130 +48,133 @@ class SignupForm extends React.Component {
       password2,
     };
 
-    this.props.signup(user, this.props.history);
-  }
+    signup(user, history);
+  };
 
-  demoLogin(e) {
-    e.preventDefault();
+  const demoLogin = () => {
 
     let user = {
       email: 'rayleensharp@gmail.com',
       password: 'password',
     };
 
-    this.props.login(user);
+    login(user);
   }
 
-  renderErrors() {
+  const renderErrors = () => {
     return (
       <ul className='errors-list'>
-        {Object.keys(this.state.errors).map((error, i) => (
+        {Object.keys(errors).map((error, i) => (
           <li>
             <Alert key={`error-${i}`} variant='warning'>
-              {this.state.errors[error]}
+              {errors[error]}
             </Alert>
           </li>
         ))}
       </ul>
     );
-  }
+  };
 
-  render() {
-    return (
-              <Card className='session signup'>
-                <Card.Title className='session'>
-                  <strong>Help keep the 415 alive. Sign up now!</strong>
-                </Card.Title>
-                {this.renderErrors()}
-                <Form onSubmit={this.handleSubmit}>
-                  <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control
-                      type="email"
-                      placeholder="Email"
-                      value={this.state.email}
-                      onChange={this.update('email')}
-                    />
-                    <Form.Text className="text-muted">
-                      We'll never share your email with anyone else.
+  return (
+    <Card className='session signup'>
+      <Card.Title className='session'>
+        <strong>Help keep the 415 alive. Sign up now!</strong>
+      </Card.Title>
+      {renderErrors()}
+      <Form onSubmit={() => handleSubmit()}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            name='email'
+            type="email"
+            placeholder="Email"
+            value={filterInput.email}
+            onChange={e => update(e)}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
                     </Form.Text>
-                  </Form.Group>
+        </Form.Group>
 
-                  <Form.Group>
-                    <Row>
-                      <Col>
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control
-                          placeholder="First Name"
-                          value={this.state.firstName}
-                          onChange={this.update('firstName')}
-                        />
-                      </Col>
-                      <Col>
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control
-                          placeholder="Last Name"
-                          value={this.state.lastName}
-                          onChange={this.update('lastName')}
-                        />
-                      </Col>
-                    </Row>
-                  </Form.Group>
-                  <Form.Group>
-                    <Row>
-                      <Col>
-                        <Form.Label>Phone Number</Form.Label>
-                        <Form.Control 
-                          type="tel"
-                          placeholder="XXX-XXX-XXXX"
-                          value={this.state.phoneNumber}
-                          onChange={this.update('phoneNumber')}
-                        />
-                      </Col>
-                    </Row>
-                  </Form.Group>
-                    <Row>
-                      <Col>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                          type="password"
-                          placeholder="Password"
-                          value={this.state.password}
-                          onChange={this.update('password')}
-                        />
-                      </Col>
-                      <Col>
-                        <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type="password"
-                          placeholder="Confirm Password"
-                          onChange={this.update('password2')}
-                        />
-                      </Col>
-                    </Row>
+        <Form.Group>
+          <Row>
+            <Col>
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                name='firstName'
+                placeholder="First Name"
+                value={filterInput.firstName}
+                onChange={e => update(e)}
+              />
+            </Col>
+            <Col>
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                name='lastName'
+                placeholder="Last Name"
+                value={filterInput.lastName}
+                onChange={e => update(e)}
+              />
+            </Col>
+          </Row>
+        </Form.Group>
+        <Form.Group>
+          <Row>
+            <Col>
+              <Form.Label>Phone Number</Form.Label>
+              <Form.Control
+                name='phoneNumber'
+                type="tel"
+                placeholder="XXX-XXX-XXXX"
+                value={filterInput.phoneNumber}
+                onChange={e => update(e)}
+              />
+            </Col>
+          </Row>
+        </Form.Group>
+        <Row>
+          <Col>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              name='password'
+              type="password"
+              placeholder="Password"
+              value={filterInput.password}
+              onChange={e => update(e)}
+            />
+          </Col>
+          <Col>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              name='password2'
+              type="password"
+              value={filterInput.password2}
+              placeholder="Confirm Password"
+              onChange={e => update(e)}
+            />
+          </Col>
+        </Row>
 
-                  <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="I agree to be a good neighbor" />
-                  </Form.Group>
-
-
-                </Form>
-                <Button
-                  className='session-btn'
-                  type="submit"
-                  onClick={this.handleSubmit}
-                  variant='secondary'>
-                  Create account
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="I agree to be a good neighbor" />
+        </Form.Group>
+      </Form>
+      <Button
+        className='session-btn'
+        type="submit"
+        onClick={() => handleSubmit()}
+        variant='secondary'>
+        Create account
                 </Button>
-                <Button
-                  className='demo-btn'
-                  type="submit"
-                  onClick={this.demoLogin}
-                  variant='secondary'>
-                  Just demo for now
+      <Button
+        className='demo-btn'
+        type="submit"
+        onClick={() => demoLogin()}
+        variant='secondary'>
+        Just demo for now
                 </Button>
-              </Card>
-    );
-  }
+    </Card>
+  );
 }
 
 export default withRouter(SignupForm);
