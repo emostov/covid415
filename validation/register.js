ca5b77ca9ed707a5406bd64fecf47387f733f124
Tarik <tariksnow37@gmail.com>
2020-07-03 22:08:26 -0400

    eslint the validation folder

--- validation/register.js
+++ validation/register.js
@@ -2,66 +2,66 @@ const Validator = require('validator');
 const validText = require('./valid-text');
 
 module.exports = function validateRegisterInput(dat) {
-  const errors = {};
-  const data = { ...dat };
+    const errors = {};
+    const data = { ...dat };
 
-  data.firstName = validText(data.firstName) ? data.firstName : '';
-  data.lastName = validText(data.lastName) ? data.lastName : '';
-  data.email = validText(data.email) ? data.email : '';
-  data.phoneNumber = validText(data.phoneNumber) ? data.phoneNumber : '';
-  data.password = validText(data.password) ? data.password : '';
-  data.password2 = validText(data.password2) ? data.password2 : '';
+    data.firstName = validText(data.firstName) ? data.firstName : '';
+    data.lastName = validText(data.lastName) ? data.lastName : '';
+    data.email = validText(data.email) ? data.email : '';
+    data.phoneNumber = validText(data.phoneNumber) ? data.phoneNumber : '';
+    data.password = validText(data.password) ? data.password : '';
+    data.password2 = validText(data.password2) ? data.password2 : '';
 
-  if (!Validator.isLength(data.firstName, { min: 1, max: 30 })) {
-    errors.firstName = 'First name must be between 1 and 30 characters';
-  }
+    if (!Validator.isLength(data.firstName, { min: 1, max: 30 })) {
+        errors.firstName = 'First name must be between 1 and 30 characters';
+    }
 
-  if (Validator.isEmpty(data.firstName)) {
-    errors.firstName = 'First name field is required';
-  }
+    if (Validator.isEmpty(data.firstName)) {
+        errors.firstName = 'First name field is required';
+    }
 
-  if (!Validator.isLength(data.lastName, { min: 1, max: 30 })) {
-    errors.lastName = 'Last name must be between 1 and 30 characters';
-  }
+    if (!Validator.isLength(data.lastName, { min: 1, max: 30 })) {
+        errors.lastName = 'Last name must be between 1 and 30 characters';
+    }
 
-  if (Validator.isEmpty(data.lastName)) {
-    errors.lastName = 'Last name field is required';
-  }
+    if (Validator.isEmpty(data.lastName)) {
+        errors.lastName = 'Last name field is required';
+    }
 
-  if (Validator.isEmpty(data.email)) {
-    errors.email = 'Email field is required';
-  }
+    if (Validator.isEmpty(data.email)) {
+        errors.email = 'Email field is required';
+    }
 
-  if (!Validator.isEmail(data.email)) {
-    errors.email = 'Email is invalid';
-  }
+    if (!Validator.isEmail(data.email)) {
+        errors.email = 'Email is invalid';
+    }
 
-  if (Validator.isEmpty(data.phoneNumber)) {
-    errors.phoneNumber = 'Phone Number field is required';
-  }
+    if (Validator.isEmpty(data.phoneNumber)) {
+        errors.phoneNumber = 'Phone Number field is required';
+    }
 
-  if (!Validator.isMobilePhone(data.phoneNumber, 'en-US')) {
-    errors.phoneNumber = 'Invalid Phone Number'
-  }
+    if (!Validator.isMobilePhone(data.phoneNumber, 'en-US')) {
+        errors.phoneNumber = 'Invalid Phone Number';
+    }
 
-  if (Validator.isEmpty(data.password)) {
-    errors.password = 'Password field is required';
-  }
+    if (Validator.isEmpty(data.password)) {
+        errors.password = 'Password field is required';
+    }
 
-  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = 'Password must be at least 6 characters';
-  }
+    if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+        errors.password = 'Password must be at least 6 characters';
+    }
 
-  if (Validator.isEmpty(data.password2)) {
-    errors.password2 = 'Confirm Password field is required';
-  }
+    if (Validator.isEmpty(data.password2)) {
+        errors.password2 = 'Confirm Password field is required';
+    }
 
-  if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = 'Passwords must match';
-  }
+    if (!Validator.equals(data.password, data.password2)) {
+        errors.password2 = 'Passwords must match';
+    }
 
-  return {
-    errors,
-    isValid: Object.keys(errors).length === 0
-  };
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0,
+    };
 };
--- validation/tasks.js
+++ validation/tasks.js
@@ -2,36 +2,36 @@ const Validator = require('validator');
 const validText = require('./valid-text');
 
 module.exports = function validateTaskInput(dat) {
-  const errors = {};
-  const data = { ...dat }; // Clone so we don't manipulate arguments
-  
-  data.type = validText(data.type) ? data.type : '';
-  data.body = validText(data.body) ? data.body : '';
-  data.deliveryAddress = validText(data.deliveryAddress) ? data.deliveryAddress : '';
-  data.deliveryInstructions = validText(data.deliveryInstructions) ? data.deliveryInstructions : '';
+    const errors = {};
+    const data = { ...dat }; // Clone so we don't manipulate arguments
 
-  if (Validator.isEmpty(data.type)) {
-    errors.text = 'Please select a task type.';
-  }
+    data.type = validText(data.type) ? data.type : '';
+    data.body = validText(data.body) ? data.body : '';
+    data.deliveryAddress = validText(data.deliveryAddress) ? data.deliveryAddress : '';
+    data.deliveryInstructions = validText(data.deliveryInstructions) ? data.deliveryInstructions : '';
 
-  if (Validator.isEmpty(data.details)) {
-    errors.text = 'Request detail is required.';
-  }
-  
-  if (!Validator.isLength(data.details, { min: 30, max: 200 })) {
-    errors.text = 'Request detail should be at least 30 characters. Please provide more details so that your volunteer has enough information to help.';
-  }
+    if (Validator.isEmpty(data.type)) {
+        errors.text = 'Please select a task type.';
+    }
 
-  if (Validator.isEmpty(data.deliveryAddress)) {
-    errors.text = 'Delivery address is required.';
-  }
+    if (Validator.isEmpty(data.details)) {
+        errors.text = 'Request detail is required.';
+    }
 
-  if (Validator.isEmpty(data.deliveryInstructions)) {
-    errors.text = 'Delivery instructions are required.';
-  }
+    if (!Validator.isLength(data.details, { min: 30, max: 200 })) {
+        errors.text = 'Request detail should be at least 30 characters. Please provide more details so that your volunteer has enough information to help.';
+    }
 
-  return {
-    errors,
-    isValid: Object.keys(errors).length === 0
-  };
+    if (Validator.isEmpty(data.deliveryAddress)) {
+        errors.text = 'Delivery address is required.';
+    }
+
+    if (Validator.isEmpty(data.deliveryInstructions)) {
+        errors.text = 'Delivery instructions are required.';
+    }
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0,
+    };
 };
