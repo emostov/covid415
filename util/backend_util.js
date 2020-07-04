42b96fc279016d58de8966bc20c40a855e73ecea
Tarik <tariksnow37@gmail.com>
2020-07-03 22:50:02 -0400

    eslint geocode file, revert for loops, for..in checks the whole prototype chain unnecesarrily

--- util/backend_util.js
+++ util/backend_util.js
@@ -1,32 +1,32 @@
 module.exports = {
-  userObjectParser: (userObject, key) => {
-    const user = {};
-    
-    for(let i in userObject) {
-      if(userObject[i] !== key) {
-        user[i] = userObject[i]
-      }
-    }
+    userObjectParser: (userObject, key) => {
+        const user = {};
+        const keys = Object.keys(userObject);
 
-    return user;
-  },
-  defreezeObject: (obj) => {
-    let tempObj = {}
+        for (let i = 0; i < keys.length; i += 1) {
+            if (userObject[keys[i]] !== key) {
+                user[keys[i]] = userObject[keys[i]];
+            }
+        }
 
-    for(let i in obj) {
-      tempObj[i] = obj[i]
-    }
+        return user;
+    },
+    defreezeObject: (obj) => {
+        const tempObj = {};
+        const keys = Object.keys(obj);
 
-    return obj
-  },
-  pullKeys: (obj) => {
-    return  {
-      "_id": obj.id,
-      "firstName": obj.firstName,
-      "lastName": obj.lastName,
-      "email": obj.email,
-      "phoneNumber": obj.phoneNumber,
-      "date": obj.date
-    }
-  }
-}
+        for (let i = 0; i < keys.length; i += 1) {
+            tempObj[keys[i]] = obj[keys[i]];
+        }
+
+        return obj;
+    },
+    pullKeys: (obj) => ({
+        _id: obj.id,
+        firstName: obj.firstName,
+        lastName: obj.lastName,
+        email: obj.email,
+        phoneNumber: obj.phoneNumber,
+        date: obj.date,
+    }),
+};
