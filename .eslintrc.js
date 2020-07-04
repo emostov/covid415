763f3cccab375558e6b25702d5f3eab81eca36d5
Tarik <tariksnow37@gmail.com>
2020-07-03 22:11:18 -0400

    eslint eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2020: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 11,
-  },
-  rules: {
-    indent: ['error', 4],
-  },
+    env: {
+        browser: true,
+        commonjs: true,
+        es2020: true,
+    },
+    extends: [
+        'airbnb-base',
+    ],
+    parserOptions: {
+        ecmaVersion: 11,
+    },
+    rules: {
+        indent: ['error', 4],
+    },
 };
